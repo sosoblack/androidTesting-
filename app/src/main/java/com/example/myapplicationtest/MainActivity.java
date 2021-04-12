6d6b7f17529dfc94523590baf2dd4a045dbb552d
sosoblack <sosoblack18@gmail.com>
2021-04-12 08:48:33 +0000

    initial Commit

--- app/src/main/java/com/example/myapplicationtest/MainActivity.java
+++ app/src/main/java/com/example/myapplicationtest/MainActivity.java
@@ -2,16 +2,19 @@ package com.example.myapplicationtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.VideoView;
 
 import pl.droidsonroids.gif.GifImageView;
 
 public class MainActivity extends AppCompatActivity {
       private Button button;
       private GifImageView gifImageView;
+      private VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,19 +23,32 @@ public class MainActivity extends AppCompatActivity {
 
         button = findViewById(R.id.button);
         gifImageView = findViewById(R.id.giff);
+        videoView = findViewById(R.id.videoView);
         gifImageView.setVisibility(View.INVISIBLE);
+
+        String path = "android.resource://com.example.myapplicationtest/"+R.raw.vid;
+        Uri uri = Uri.parse(path);
+        videoView.setVideoURI(uri);
+
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+
                 gifImageView.setVisibility(View.VISIBLE);
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
+
+               // new Handler().postDelayed(new Runnable() {
+               //     @Override
+                   // public void run() {
                         gifImageView.setVisibility(View.INVISIBLE);
-                        finish();
-                    }
-                }, 500);
+                        videoView.start();
+                       // finish();
+
+                 //   }
+                //}, 2000);
+               // videoView.seekTo(000);
 
             }
         });
